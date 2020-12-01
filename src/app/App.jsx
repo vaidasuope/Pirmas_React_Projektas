0dfbb2c8e36ff5c88438664569d66c9214f3c484
vaidasuope <vaida.dabasinskaite@gmail.com>
2020-12-01 13:50:39 +0200

    naujas komponentas sukurtas

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App () {
+    return (
+        <div>
+            <p>Vaidos page</p>
+            <h5>Antraste</h5>
+        </div>
+    )
+}
+
+export default App
